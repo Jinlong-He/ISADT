6405ca267609c31d8cb8f3fd1b1dd34240947248
JinlongHe <hejl@Mac-mini.local>
2020-07-29 12:28:41 +0800

    arg parser

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,10 @@
 #include "../include/Parser/LParser/LParser.hpp"
 #include "../include/CodeGenerator/CCodeGenerator.hpp"
 #include "../include/Manage.hpp"
+#include "../include/Parser/ArgParser/argparse.hpp"
 using namespace std;
 using namespace isadt;
+using namespace argparse;
 
 unordered_set<string> Manage::eqOps_({"==", "!=", ">", ">=", "<", "<="});
 unordered_set<string> Manage::arOps_({"+", "-", "*", "/"});
@@ -25,29 +27,72 @@ unordered_map<string, UserType*> Manage::typeMap_({{"int", Manage::intType_},
 
 int main(int argc, char *argv[]) {
     try {
+        ArgumentParser program("isadt");
+        program.add_argument("-i", "--input")
+            .required()
+            .nargs(1)
+            .help("the input file of model");
+        program.add_argument("-v", "--verify")
+            .help("verify the properties of a given model")
+            .default_value(false)
+            .implicit_value(true);
+        program.add_argument("-g", "--generate")
+            .help("generate the codes of a given model")
+            .default_value(false)
+            .implicit_value(true);
+        program.add_argument("-engine=beagle")
+            .help("set the engine for verifier")
+            .default_value(false)
+            .implicit_value(true);
+        program.add_argument("-engine=proverif")
+            .help("set the proverif engine for verifier")
+            .default_value(false)
+            .implicit_value(true);
+        program.add_argument("-engine=simulation")
+            .help("set the simulation engine for generator")
+            .default_value(false)
+            .implicit_value(true);
+        program.add_argument("-engine=real")
+            .help("set the real engine for generator")
+            .default_value(false)
+            .implicit_value(true);
+        try {
+            program.parse_args(argc, argv);
+        } catch (const std::runtime_error& err) {
+          std::cout << err.what() << std::endl;
+          std::cout << program;
+          exit(0);
+        }
+        auto file = program.get<vector<string>>("-i")[0];
         Model model;
-        XmlParser::parse(argv[1], &model);
-        auto proc = model.getProcesses().front();
-        auto sm = proc -> getStateMachines().front();
-        sm -> print();
-        cout << endl;
-        auto sm1 = proc -> mkBeagleStateMachine(sm);
-        sm1 -> print();
-        //Process* p = model.mkProcess("TestProc");
-        //UserType* type = new UserType();
-        //type->setName("Integer");
-        //p->mkAttribute(type, "myInt");
-        //p->mkMethod("testMethod", type, "", "/*Here is the user code*/\n");
-        //p->mkMethod("cryptMethod", type, "1", "");
-        //p->mkCommMethod("commMethod", type, true, "id1");
-        //StateMachine* sm = p->mkStateMachine(new Vertex("INIT"));
-        //Vertex* first = sm->mkVertex("FIRST");
-        //Vertex* second = sm->mkVertex("SECOND");
-        //Vertex* end = sm->mkEndVertex("END");
-        //sm->mkEdge(first, second)->addAction(new Action());
-        //sm->mkEdge(second, end)->addAction(new Action());
-        //CCodeGenerator* codegen = new CCodeGenerator();
-        //codegen->generateCodeProc("/Users/hejl/Projects/ISADT/generated", model.getProcesses().front());
+        XmlParser::parse(file.c_str(), &model);
+        if (program["-v"] == true) {
+            cout << "-v" << endl;
+            if (program["-engine=proverif"] == true) {
+            } else if (program["-engine=beagle"] == true) {
+                //this is an example for make a statemahine for beagle.
+                auto proc = model.getProcesses().front();
+                auto sm = proc -> getStateMachines().front();
+                auto bsm = proc -> mkBeagleStateMachine(sm);
+                bsm -> print();
+            } else {
+                //this is an example for make a statemahine for beagle.
+                auto proc = model.getProcesses().front();
+                auto sm = proc -> getStateMachines().front();
+                auto bsm = proc -> mkBeagleStateMachine(sm);
+                bsm -> print();
+            }
+        }
+        if (program["-g"] == true) {
+            cout << "-g" << endl;
+            if (program["-engine=simulation"] == true) {
+                cout << "simulation" << endl;
+            } else if (program["-engine=real"] == true) {
+                cout << "real" << endl;
+            } else {
+                cout << "simulation" << endl;
+            }
+        }
     } catch (string e) {
         cout << e << endl;
         exit(1);
